32b59458e3559adad1e01648b695482d03049eb5
maahin2005 <mahin.malek2005@gmail.com>
2023-10-25 10:36:02 +0530

    DAY-1-React-Learning

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home";
+import Home1 from "./components/Home1";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My React Learning Journey</p>
+        <Home />
+        <Home1 />
       </header>
     </div>
   );
---
+++ src/components/Home1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1>This is My function based Home Component</h1>
+    </>
+  );
+}
