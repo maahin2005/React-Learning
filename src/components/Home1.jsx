19016f2e97b8253bd2f851ebc998df94c1e08a1b
maahin2005 <mahin.malek2005@gmail.com>
2023-10-29 12:46:16 +0530

    Practice the toggle systems

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function Home(props) {
+  const [state, setState] = useState(true);
   return (
     <>
       <h1>This is My function based Home Component</h1>
       <p>Hello, {props.name}</p>
+      <hr />
+      {state ? (
+        <h2>Hide & Show - Toggle Practice with Function-based Comp.</h2>
+      ) : null}
+      <button
+        onClick={() => {
+          setState(!state);
+        }}
+      >
+        {" "}
+        <h4>Toggle me</h4>{" "}
+      </button>
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,31 @@
 import { React, Component } from "react";
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      flag: true,
+    };
+  }
   render() {
     return (
       <>
         <h1>This is my class based Home component</h1>
         <p>Hello, {this.props.name}</p>
+        <hr />
+        {this.state.flag === true ? (
+          <h2>Hide & Show - Toggle Practice with class-based Comp.</h2>
+        ) : null}
+        <button
+          onClick={() => {
+            this.setState({
+              flag: !this.state.flag,
+            });
+          }}
+        >
+          {" "}
+          <h4>Toggle me</h4>{" "}
+        </button>
       </>
     );
   }
